ed1efd12a032fe2675362c66ff713627bf9613d5
Malcolm Smalls <smallsbeats@gmail.com>
2022-09-03 10:42:09 -0700

    Finish 'Multiply numeric property values by 2' exercise

--- js/main.js
+++ js/main.js
@@ -67,3 +67,38 @@ for (let key in salaries){
 }
 
 console.log(sum)
+
+// Multiply numeric property values by 2
+// importance: 3
+// Create a function multiplyNumeric(obj) that multiplies all numeric property values of obj by 2.
+
+// For instance:
+
+// // before the call
+let menu = {
+  width: 200,
+  height: 300,
+  title: "My menu"
+};
+
+// multiplyNumeric(menu);
+
+// // after the call
+// menu = {
+//   width: 400,
+//   height: 600,
+//   title: "My menu"
+// };
+// Please note that multiplyNumeric does not need to return anything. It should modify the object in-place.
+
+// P.S. Use typeof to check for a number here.
+
+function multiplyNumeric(obj){
+    for (let number in menu){
+        if(typeof(menu[number]) === "number"){
+            menu[number] = menu[number] * 2
+        }
+    }
+}
+
+multiplyNumeric(menu)
